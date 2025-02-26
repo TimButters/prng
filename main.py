fdd61257d5f14a722afe77b6872cf9735da2c9fa
Tim Butters <tim.d.butters@gmail.com>
2025-02-26 16:29:12 +0000

    Added some more tests?? I haven't touched this for years

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+import scipy as spc
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,6 +15,31 @@ def spectral_test(prng, *, num_points=1000, dim=3):
     ax = fig.add_subplot(projection="3d")
     ax.scatter(points[:, 0], points[:, 1], points[:, 2], marker="o")
     return points
+
+
+def to_binary(number, only_decimal=True):
+    if isinstance(number, int):
+        return format(number, "b")
+    if isinstance(number, float):
+        a, b = str(number).split(".")
+        if only_decimal:
+            return format(int(b), "b")
+        return "".join([format(int(n), "b") for n in [a, b]])
+    return ValueError("input must be an int or a float.")
+
+
+def monobit_test(number):
+    if isinstance(number, int):
+        binary = [int(d) for d in list(to_binary(number))]
+    elif isinstance(number, list):
+        binary = [int(d) for n in number for d in list(to_binary(n))]
+    else:
+        raise ValueError("Input must be an int or list of ints.")
+
+    bit_length = len(binary)
+    total = 2*sum(binary) - bit_length
+    sobs = total / math.sqrt(bit_length)
+    return math.erfc(math.fabs(sobs) / math.sqrt(2))
 
 
 if __name__ == "__main__":
